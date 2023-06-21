07d29a11517c0694cac3b095c33a39a77296797f
Yohan_Garcia <75887329+yohanJS@users.noreply.github.com>
2023-06-21 18:05:48 -0400

    Add project files.

---
+++ Data/MVCBlogContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCBlog.Models;
+
+namespace MVCBlog.Data
+{
+    public class MVCBlogContext : DbContext
+    {
+        public MVCBlogContext (DbContextOptions<MVCBlogContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCBlog.Models.Blog> Blog { get; set; } = default!;
+    }
+}
---
+++ Models/Blog.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCBlog.Models
+{
+    public class Blog
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        public string? Description { get; set; } = null;
+        [Required]
+        public string? Author { get; set; }
+        public string? Content { get; set; }
+
+        //The DataType attribute on CreatedDate specifies the type of the data (Date). With this attribute:
+        [DataType(DataType.Date)]
+        public DateTime CreatedDate { get; set;} = DateTime.Now;
+
+    }
+}
